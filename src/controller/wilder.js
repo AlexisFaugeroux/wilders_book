17482aebb839a427812339019589be808e25e289
AdrienSergentWildCodeSchool <adrien.sergent@wildcodeschool.com>
2023-04-25 16:58:54 +0200

    add entity Skill and relations with Wilder

--- src/controller/wilder.js
+++ src/controller/wilder.js
@@ -1,15 +1,67 @@
 const dataSource = require("../utils").dataSource;
 const Wilder = require("../entity/Wilder")
+const Skill = require("../entity/Skill")
 module.exports = {
-    create: (req, res) => {
-        dataSource
+    create: async (req, res) => {
+        try {
+            await dataSource
             .getRepository(Wilder)
-            .save(req.body)
-            .then( () => {
-                res.send("Created wilder");
-            })
-            .catch(() => {
+            .save(req.body);
+            res.send("Created wilder");
+            
+        } catch(error) {
+            console.log(error);
+            res.send("Error while creating wilder");
+            }
+    },
+    read: async (req, res) => {
+        try {
+            const data = await dataSource
+            .getRepository(Wilder)
+            .find()      
+            res.send(data);
+        } catch(error) {
                 res.send("Error while creating wilder");
-            })
+            }
+    },
+    update: async (req, res) => {
+        try {
+            await dataSource
+            .getRepository(Wilder)
+            .update(req.body.id, req.body.newData)
+            
+                res.send("updated");
+        }catch(error) {
+                res.send("Error while creating wilder");
+            }
+    },
+    delete: async (req, res) => {
+        try {
+            await dataSource
+            .getRepository(Wilder)
+            .delete(req.body)
+                res.send("deleted");
+        }catch(error) {
+                res.send("Error while creating wilder");
+            }
+    },
+    addSkill: async (req, res) => {
+        try {
+            const wilderToUpdate = await dataSource
+            .getRepository(Wilder)
+            .findOneBy({name: req.body.wilderName});
+            console.log(wilderToUpdate);
+
+            const skillToAdd = await dataSource
+            .getRepository(Skill)
+            .findOneBy({name: req.body.skillName});
+
+            wilderToUpdate.skills = [...wilderToUpdate.skills, skillToAdd];
+            await dataSource.getRepository(Wilder).save(wilderToUpdate)
+            res.send("Skill added to wilder");
+
+        }catch(error) {
+                res.send("Error while creating skill");
+            }
     }
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const dataSource = require("./utils").dataSource;
 const WilderController = require("./controller/wilder");
+const SkillController = require("./controller/skill")
 
 const app = express();
 
@@ -11,6 +12,20 @@ app.get("/", (req, res) => {
 })
 
 app.post("/api/wilder", WilderController.create);
+app.get("/api/wilder", WilderController.read);
+app.put("/api/wilder", WilderController.update);
+app.delete("/api/wilder", WilderController.delete);
+app.put("/api/addSkill", WilderController.addSkill)
+
+app.post("/api/skill", SkillController.create);
+app.get("/api/skill", SkillController.read);
+app.put("/api/skill", SkillController.update);
+app.delete("/api/skill", SkillController.delete);
+
+
+app.use((req, res, next) => {
+    res.status(404).send("Sorry can't find that !");
+})
 
 const start = async () => {
     await dataSource.initialize();
