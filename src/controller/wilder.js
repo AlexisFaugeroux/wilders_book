8ab48213317f0ef22237b0809ac213fc9782bbb2
AdrienSergentWildCodeSchool <adrien.sergent@wildcodeschool.com>
2023-04-26 11:47:22 +0200

    add grades and update relations tables

--- src/controller/wilder.js
+++ src/controller/wilder.js
@@ -1,6 +1,7 @@
 const dataSource = require("../utils").dataSource;
 const Wilder = require("../entity/Wilder")
 const Skill = require("../entity/Skill")
+const Grade = require("../entity/Grade")
 module.exports = {
     create: async (req, res) => {
         try {
@@ -16,11 +17,31 @@ module.exports = {
     },
     read: async (req, res) => {
         try {
-            const data = await dataSource
-            .getRepository(Wilder)
-            .find()      
-            res.send(data);
+            const grades = await dataSource.getRepository(Grade).find();
+            console.log(grades);
+
+            const wilders = await dataSource.getRepository(Wilder).find();
+            console.log(wilders)
+
+            const data = wilders.map((wilder) => {
+                const wilderGrades = grades.filter(
+                    (grade) => grade.wilder.id === wilder.id
+                )
+                const wilderGradeLean = wilderGrades.map((el) => {
+                    return { title: el.skill.name, votes: el.grade};
+                });
+                const result = {
+                    ...wilder,
+                    skills: wilderGradeLean,
+                }
+                console.log(result);
+                return result
+
+            })
+            res.send(data)
+
         } catch(error) {
+                console.log(error)
                 res.send("Error while creating wilder");
             }
     },
--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 const express = require("express");
+const cors = require("cors")
 const dataSource = require("./utils").dataSource;
 const WilderController = require("./controller/wilder");
 const SkillController = require("./controller/skill")
+const GradeController = require("./controller/grade")
 
 const app = express();
 
 app.use(express.json());
+app.use(cors({ origin: "http://localhost:3000"}))
 
 app.get("/", (req, res) => {
     res.send("Hello World");
@@ -15,12 +18,15 @@ app.post("/api/wilder", WilderController.create);
 app.get("/api/wilder", WilderController.read);
 app.put("/api/wilder", WilderController.update);
 app.delete("/api/wilder", WilderController.delete);
-app.put("/api/addSkill", WilderController.addSkill)
+
 
 app.post("/api/skill", SkillController.create);
 app.get("/api/skill", SkillController.read);
 app.put("/api/skill", SkillController.update);
 app.delete("/api/skill", SkillController.delete);
+
+app.get("/api/grade", GradeController.read)
+app.post("/api/grade", GradeController.create)
 
 
 app.use((req, res, next) => {
@@ -29,6 +35,6 @@ app.use((req, res, next) => {
 
 const start = async () => {
     await dataSource.initialize();
-    app.listen(3000, () => console.log("Server started on 3000"));
+    app.listen(5000, () => console.log("Server started on 5000"));
 }
 start(); 
